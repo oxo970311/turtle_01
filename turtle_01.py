d0635b21a84fe80ad0a8ea3276f5969bf92dfa50
oxo970311 <148974384+oxo970311@users.noreply.github.com>
2025-07-22 10:36:40 +0900

    Update turtle_01.py

--- turtle_01.py
+++ turtle_01.py
@@ -11,6 +11,6 @@ t.penup();t.goto(-300, -300);t.pendown()
 
 t.penup();t.goto(300, 300);t.pendown()
 
-s.penup();s.goto(50, -50);s.pendown();s.goto(-50, 50)
+s.pensize(5);s.penup();s.goto(50, -50);s.pendown();s.goto(-50, 50)
 
 turtle.done()
