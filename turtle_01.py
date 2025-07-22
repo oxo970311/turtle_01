944162e116a4dab8dd941450327a9a9bc0323e32
oxo970311 <148974384+oxo970311@users.noreply.github.com>
2025-07-22 10:55:58 +0900

    장애물피하기
    장애물피하기

--- turtle_01.py
+++ turtle_01.py
@@ -13,4 +13,27 @@ t.penup();t.goto(300, 300);t.pendown()
 
 s.pensize(5);s.penup();s.goto(50, -50);s.pendown();s.goto(-50, 50)
 
+t.rt(180)
+
+while True:
+    t.penup()
+    t.forward(5)
+    x, y = t.pos()
+
+    if -50 <= x <= 50 and -50 <= y <= 50:
+        t.rt(90)
+        t.forward(100)
+        t.lt(90)
+        t.forward(100)
+        t.lt(90)
+        t.forward(100)
+        t.right(90)
+        t.forward(350)
+        break
+
+    t.pendown()
+
+
+
 turtle.done()
+
